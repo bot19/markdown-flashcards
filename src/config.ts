4aa6d2ff94f4f22332b937b9c974451dcfb01604
bot19 <jsbot@pm.me>
2024-04-27 22:11:33 +1000

    update app info to deck/dictionary

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const APP_CONFIG = {
-  questionsEachSession: 3,
-  shuffleQuestions: false, // on false, will sort in alphabetical order
+  questionsEachSession: 10,
+  shuffleQuestions: true, // on false, will sort in alphabetical order
   quizInfo: {
-    name: "Markdown Flashcards",
+    name: "I R spek Engrish",
     description:
-      "Build flashcards from your Markdown files. Use this app to help commit info from your knowledge base to long-term memory.",
+      "Mastering English words that elude me so I don't feel like I have imposter syndrome as a native speaker.",
     author: "bot19",
-    year: "2023",
+    year: "2024",
     link: "https://github.com/bot19/markdown-flashcards",
     linkText: "Github",
-    version: "0.1.0",
+    version: "1.0.0",
   },
 };
