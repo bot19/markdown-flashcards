8d285cab374fecb7f9c59e732117a413a8b00590
bot19 <jsbot@pm.me>
2024-05-03 15:52:46 +1000

    config update + version update logic notes

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,17 @@ export const APP_CONFIG = {
     year: "2024",
     link: "https://github.com/bot19/markdown-flashcards",
     linkText: "Github",
-    version: "1.0.0",
-    build: 1,
+    version: "1.5.1", // see [1] for odd logic
+    build: 1, // increment on data change, verify data deployed
   },
 };
+
+/**
+ * [1] logic
+ * not using proper logic
+ * each code change I will increment the patch number
+ * major/minor number change occurs when deemed enough updates added
+ *
+ * so 1.5.0 might not look very different to 1.0.0, or 1.5.50 vs 2.0.0
+ * odd yes, but doing it for branching purposes - keep major/minor branch
+ */
